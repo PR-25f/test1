fadad095bc1a4bf0eb42c690f115d11d500f97f2
PUSHKAR <ec2-user@ip-172-31-2-119.ap-south-1.compute.internal>
2024-09-11 09:45:28 +0000

    my script file

---
+++ file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo " The number is: "
+num=2421
+count=0
+while [ $num -gt 0 ]
+do
+        rem=$(($num % 10))
+        ((count++))
+        num=$(($num / 10))
+done
+echo " the count is $count "
