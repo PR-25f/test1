1b10704abb5481f5825fb3b1da41ab2495f9fdb6
PUSHKAR RANJAN <pushkarranjan.2502@gmail.com>
2024-09-11 15:50:30 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -2,10 +2,10 @@
 echo " The number is: "
 num=2421
 count=0
-while ( 2421 > 0 )
+while [ $num -gt 0 ]
 do
-        rem=$((2421 % 10))
+        rem=$(($num % 10))
         ((count++))
-        num=$((2421 / 10))
+        num=$(($num / 10))
 done
 echo " the count is $count "
